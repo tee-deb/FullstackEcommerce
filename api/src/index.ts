d3057f1be9941caa287306276c96fbb6b2c4dab3
tee-deb <solodey4uu@gmail.comm>
2024-12-13 06:25:04 -0800

    Set up API and Express

---
+++ api/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const port = 3000;
+
+const app = express();
+
+
+app.get('/',  (req, res) => {
+    res.send('Hello World!')
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app is running on port ${port}`)
+  });
+
